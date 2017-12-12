0932266bfd4182517599b9030c2ed328be8bc368
DANJOUX MICKAEL p1602272 <mickael.danjoux@etu.univ-lyon1.fr>
2017-12-12 10:08:38 +0100

    classe Commentaire

---
+++ src/Models/Commentaire.php
@@ -0,0 +1,52 @@
+<?php
+	
+	class Commentaire 
+	{
+		private $idCommentaire;
+		private $idArticle;
+		private $nomEditeur;
+		private $contenuCommentaire;
+
+		function __construct($idCommentaire,$idArticle,$nomEditeur,$ContenuCommentaire)
+		{
+			$this->idCommentaire=$_idCommentaire;
+			$this->idArticle=$_idArticle;
+			$this->nomEditeur=$_nomEditeur;
+			$this->contenuCommentaire=$_contenurCommentaire;
+			
+		}
+
+		public function getIdCommentaire(){
+			return $this->idCommentaire;
+		}
+
+		public function getIdArticle(){
+			return $this->idArticle;
+		}
+
+		public function getNomEditeur(){
+			return $this->nomEditeur;
+		}
+
+		public function getContenuCommentaire(){
+			return $this->contenuCommentaire;
+		}
+
+
+		public function setIdCommentaire($idCommentaire){
+			 $this->idCommentaire=$idCommentaire;
+		}
+
+		public function setIdArticle($idArticle){
+			$this->idArticle=$idArticle;
+		}
+
+		public function setNomEditeur($nomEditeur){
+			 $this->nomEditeur=$nomEditeur;
+		}
+
+		public function setContenuCommentaire($contenuCommentaire){
+			 $this->contenuCommentaire=$contenuCommentaire;
+		}
+	}
+  ?>
